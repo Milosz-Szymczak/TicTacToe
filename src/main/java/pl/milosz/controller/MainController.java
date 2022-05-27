0ff8f935ec3ea8413233ae40a178732e4d69b6c5
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2022-05-27 20:03:10 +0200

    add setter Sign

--- src/main/java/pl/milosz/controller/MainController.java
+++ src/main/java/pl/milosz/controller/MainController.java
@@ -1,8 +1,13 @@
 package pl.milosz.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainController {
 
@@ -45,8 +50,37 @@ public class MainController {
     @FXML
     private Button restartButton;
 
-    public void initialize() {
+    private ArrayList<Button> buttons;
+    private int playerMove = 0;
 
+    public void initialize() {
+        buttons = new ArrayList(Arrays.asList
+                (button00, button01, button02, button10, button11, button12, button20, button21, button22));
+
+        buttons.forEach(this::setupButton);
+
+    }
+
+    private void setupButton(Button button) {
+        button.addEventFilter(ActionEvent.ACTION, actionEvent -> {
+            setSign(button);
+            button.setDisable(true);
+            checkIfGameIsOver();
+        });
+    }
+
+    private void checkIfGameIsOver() {
+
+    }
+
+    private void setSign(Button button) {
+        if (playerMove == 0) {
+            button.setText("X");
+            playerMove = 1;
+        } else {
+            button.setText("O");
+            playerMove = 0;
+        }
     }
 
 }
